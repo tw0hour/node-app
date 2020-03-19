38ebdb350f3e1dc2e4ee8a72594a982204328111
tw0hour <ismael.abdallah10@gmail.com>
2020-03-19 15:53:29 +0100

    creating route /ville and coorrecting the views

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node-app' });
 });
 
+router.get('/ville', function(req, res, next) {
+  res.render('ville', { ville: req.query.nom_ville });
+});
+
 module.exports = router;
